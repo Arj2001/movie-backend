54ec4a0c072c602432893da012a0a32cd1d4f991
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-19 14:25:29 +0530

    resource

--- src/main/java/com/nestdigital/moviebackend/controller/MovieController.java
+++ src/main/java/com/nestdigital/moviebackend/controller/MovieController.java
@@ -1,13 +1,24 @@
 package com.nestdigital.moviebackend.controller;
 
+import com.nestdigital.moviebackend.dao.MovieDao;
+import com.nestdigital.moviebackend.model.MovieModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieController {
 
-    @PostMapping("/movieAdd")
-    public String movie(){
-        return "text";
+    @Autowired
+    private MovieDao dao;
+
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/movieAdd",consumes = "application/json",produces = "application/json")
+    public String movie(@RequestBody MovieModel movie){
+        dao.save(movie);
+        return "{status:'success'}";
     }
 }
